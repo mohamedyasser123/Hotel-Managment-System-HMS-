8cd1c6327c7554bc73c0574900a690034dff3811
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-06 18:03:20 +0300

    rest password done without api

---
+++ src/api/axoisClient.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  //baseURL: "https://upskilling-egypt.com:3003/api/v1", 
+  timeout: 5000,
+});
+
+// Request Interceptor
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// Response Interceptor
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const token = localStorage.getItem("token");
+
+    if (error.response?.status === 401 && token) {
+      localStorage.clear();
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
