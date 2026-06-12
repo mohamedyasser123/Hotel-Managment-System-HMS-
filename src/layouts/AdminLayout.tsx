60686cb9fd481996c05398f87d728bdd2caceb97
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-13 01:45:47 +0300

    admin layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Sidebar from '../modules/Shared/Components/Sidebar/Sidebar';
+import AdminHeader from '../modules/Shared/Components/AdminHeader/AdminHeader';
 
 export default function AdminLayout() {
-
-    
   return (
+    <Box sx={{ display: 'flex', height: '100vh', width: '100vw', overflow: 'hidden' }}>
+      
+      <Box component="aside" sx={{ height: '100%', flexShrink: 0 }}>
+         <Sidebar /> 
+      </Box>
 
-    // Navbar SideBar Outlet
-    <>
-    <div>AdminLayout</div>
-    <Outlet/>
-        </>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          height: '100%',
+          overflow: 'hidden',
+        }}
+      >
+        <Box component="header" sx={{ width: '100%', flexShrink: 0 }}>
+        <AdminHeader/>
+        </Box>
 
-  )
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,          
+            bgcolor: '#FFFFFF',
+            overflowY: 'auto',
+          }}
+        >
+          <Outlet />
+        </Box>
+      </Box>
+
+    </Box>
+  );
 }
