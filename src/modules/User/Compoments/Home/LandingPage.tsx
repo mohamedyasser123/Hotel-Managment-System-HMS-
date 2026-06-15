bd5d9ace53fbdbdfa10bf72a1a5563f53a7f1912
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-16 02:04:09 +0300

    start sprint 3 is done

--- src/modules/User/Compoments/Home/LandingPage.tsx
+++ src/modules/User/Compoments/Home/LandingPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import { Box } from '@mui/material'
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
+   <Box
+  sx={{
+    p: 3,
+  }}
+>
+{/* hero */}
+
+{/* most pouplar */}
+
+{/* house slider */}
+
+{/* hotel slider */}
+
+{/* ads section */}
+
+{/* review section */}
+
+</Box>
   )
 }
