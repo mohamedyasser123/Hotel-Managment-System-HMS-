852ac16364a5a61084275edad5526514073ddb30
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-16 16:54:38 +0300

    user layout

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Navbar from '../modules/Shared/Components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../modules/Shared/Components/Footer/Footer'
+import { Box } from '@mui/material';
 
 export default function UserLayout() {
   return (
-    // header Footer outlet  
-    <div>UserLayout</div>
-  )
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Navbar />
+
+      <Box
+        component="main"
+        sx={{
+          flex: 1,
+        }}
+      >
+        <Outlet />
+      </Box>
+
+      <Footer />
+    </Box>
+  );
 }
