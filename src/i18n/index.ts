c41118a28ad83e104228a7be646ad5e5d762fed5
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-06-18 19:17:36 +0300

    use translate

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,16 +2,24 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next"; 
 
 import enAuth from "../locales/en/auth.json";
+import enUser from "../locales/en/user.json"
+import enAdmin from "../locales/en/admin.json"
 import arAuth from "../locales/ar/auth.json";
+import arUser from "../locales/ar/user.json";
+import arAdmin from "../locales/ar/admin.json";
 
 i18n.use(initReactI18next).init({
   resources: {
     en: {
       auth: enAuth,
+      user: enUser,
+      admin: enAdmin,
     },
 
     ar: {
       auth: arAuth,
+      user: arUser,
+      admin: arAdmin,
     },
   },
 
